cfff6d80a5ec12e5216bdb82364b671901f27cd6
addislw <addislw@gmail.com>
2020-11-25 20:55:03 -0500

    Completed array exercise

--- array_exercises.rb
+++ array_exercises.rb
@@ -25,3 +25,238 @@ print doubler([1, 2, 3, 4]) # => [2, 4, 6, 8]
 puts
 print doubler([7, 1, 8])    # => [14, 2, 16]
 puts
+
+
+
+# Write a method yell(words) that takes in an array of words and returns a new array where every word from the original array has an exclamation point after it
+
+def yell(words)
+  return words.map { |word| "#{word}!" }
+end
+
+print yell(["hello", "world"]) # => ["hello!", "world!"]
+puts
+print yell(["code", "is", "cool"]) # => ["code!", "is!", "cool!"]
+puts
+
+
+
+# Write a method element_times_index(nums) that takes in an array of numbers and returns a new array containing every number of the original array multiplied with its index
+
+# different implementation
+=begin
+def element_times_index(nums)
+  return nums.map! { |num| num * nums.index(num) }
+end
+=end
+
+def element_times_index(nums)
+  new_nums = []
+  nums.each_with_index { |ele, idx| new_nums << ele * idx }
+  new_nums
+end
+
+print element_times_index([4, 7, 6, 5])       # => [0, 7, 12, 15]
+puts
+print element_times_index([1, 1, 1, 1, 1, 1]) # => [0, 1, 2, 3, 4, 5]
+puts
+
+
+
+# Write a method even_nums(max) that takes in a number max and returns an array containing all even numbers from 0 to max
+
+def even_nums(max)
+  nums_arr = []
+  (0..max).each { |num| num % 2 == 0 ? nums_arr << num : next }
+  nums_arr
+end
+
+print even_nums(10) # => [0, 2, 4, 6, 8, 10]
+puts
+print even_nums(5)  # => [0, 2, 4]
+puts
+
+
+
+# Write a method range(min, max) that takes in two numbers min and max. The method should return an array containing all numbers from min to max inclusive
+
+# compact/best solution 
+=begin
+def range(min, max)
+  (min..max).to_a
+end
+=end
+
+def range(min, max)
+  num_arr = []
+
+  i = min
+  while i <= max
+    num_arr << i
+    i += 1
+  end
+
+  num_arr
+end
+
+print range(2, 7)   # => [2, 3, 4, 5, 6, 7]
+puts 
+print range(13, 20) # => [13, 14, 15, 16, 17, 18, 19, 20]
+puts
+
+
+
+# Write a method odd_range(min, max) that takes in two numbers min and max. The method should return an array containing all odd numbers from min to max (inclusive)
+
+# solution using range
+def odd_range(min, max)
+  return (min..max).select { |num| num % 2 != 0 }
+end
+
+# solution using inject (BROKEN)
+=begin
+def odd_range(min, max)
+  num_arr = (min..max).inject([]) { |arr, num| arr << num if num % 2 != 0 }
+end
+=end
+
+print odd_range(11, 18) # => [11, 13, 15, 17]
+puts
+print odd_range(3, 7)   # => [3, 5, 7]
+puts
+
+
+
+
+# Write a method reverse_range(min, max) that takes in two numbers min and max. The function should return an array containing all numbers from min to max in reverse order. The min and max should be excluded from the array
+
+# short solution
+=begin
+def reverse_range(min, max)
+  arr = (min + 1...max).to_a.reverse
+end
+=end
+
+def reverse_range(min, max)
+  num_arr = []
+
+  i = max - 1 
+  while i > min
+    num_arr << i
+    i -= 1
+  end
+
+  num_arr
+end
+
+print reverse_range(10, 17) # => [16, 15, 14, 13, 12, 11]
+puts
+print reverse_range(1, 7)   # => [6, 5, 4, 3, 2]
+puts
+
+
+
+
+
+# Write a method first_half(array) that takes in an array and returns a new array containing the first half of the elements in the array. If there is an odd number of elements, return the first half including the middle element
+
+def first_half(array)
+  new_arr = []
+  length = array.length / 2.0
+
+  array.each_with_index do |num, index|
+    new_arr << num if index < length
+  end
+
+  new_arr
+end
+
+print first_half(["Brian", "Abby", "David", "Ommi"]) # => ["Brian", "Abby"]
+puts
+print first_half(["a", "b", "c", "d", "e"])          # => ["a", "b", "c"]
+puts
+
+
+
+
+# Write a method factors_of(num) that takes in a num and returns an array of all positive numbers less than or equal to num that can divide num
+
+def factors_of(num)
+  new_arr = (1..num).select { |i| num % i == 0 }
+end
+
+print factors_of(3)   # => [1, 3]
+puts
+print factors_of(4)   # => [1, 2, 4]
+puts
+print factors_of(8)   # => [1, 2, 4, 8]
+puts
+print factors_of(9)   # => [1, 3, 9]
+puts
+print factors_of(16)  # => [1, 2, 4, 8, 16]
+puts
+
+
+
+
+# Write a method select_odds(numbers) that takes in an array of numbers and returns a new array containing the odd numbers of the original array
+
+def select_odds(numbers)
+  new_numbers = numbers.select { |num| !num.even? }
+end
+
+print select_odds([13, 4, 3, 7, 6, 11]) # => [13, 3, 7, 11]
+puts
+print select_odds([2, 4, 6])            # => []
+puts
+
+
+
+
+# Write a method select_long_words(words) that takes in an array of words and returns a new array containing all of the words of the original array that are longer than 4 characters.
+
+def select_long_words(words)
+  new_words = words.select { |word| word.length > 4 }
+end
+
+print select_long_words(["what", "are", "we", "eating", "for", "dinner"]) # => ["eating", "dinner"]
+puts
+print select_long_words(["keep", "coding"])                               # => ["coding"]
+puts
+
+
+
+
+# Write a method sum_elements(arr1, arr2) that takes in two arrays. The method should return a new array containing the results of adding together corresponding elements of the original arrays. You can assume the arrays have the same length.
+
+def sum_elements(arr1, arr2)
+  new_arr = []
+
+  i = 0 
+  while i < arr1.length
+    new_arr << (arr1[i] + arr2[i])
+    i +=1
+  end
+
+  new_arr
+end
+
+print sum_elements([7, 4, 4], [3, 2, 11])                            # => [10, 6, 15]
+puts
+print sum_elements(["cat", "pizza", "boot"], ["dog", "pie", "camp"]) # => ["catdog", "pizzapie", "bootcamp"]
+puts
+
+
+
+# Write a method fizz_buzz(max) that takes in a number max and returns an array containing all numbers greater than 0 and less than max that are divisible by either 4 or 6, but not both
+
+def fizz_buzz(max)
+  nums_arr = (1...max).select do |num|
+    (num % 4 == 0 || num % 6 == 0) && !(num % 4 == 0 && num % 6 == 0)
+  end
+end
+
+print fizz_buzz(20) # => [4, 6, 8, 16, 18]
+puts
+print fizz_buzz(15) # => [4, 6, 8]
+puts
