5e80a3b327a19f4268837efee88d40278fca1d8e
addislw <addislw@gmail.com>
2020-11-29 00:40:42 -0500

    All unique pairs summed and evaluated for empty sum

--- 2D_array_exercises.rb
+++ 2D_array_exercises.rb
@@ -23,3 +23,20 @@ puts
 
 print combinations([0, 1, 2, 3]); # => [ [ 0, 1 ], [ 0, 2 ], [ 0, 3 ], [ 1, 2 ], [ 1, 3 ], [ 2, 3 ] ]
 puts
+
+
+
+# Write a method opposite_count that takes in an array of unique numbers. The method should return the number of pairs of elements that sum to 0
+
+# Solution 1
+def opposite_count(array)
+  count = 0 
+  array.combination(2).to_a.each do |nums|
+    count += 1 if nums.sum == 0
+  end
+
+  count
+end
+
+p opposite_count([ 2, 5, 11, -5, -2, 7 ]) # => 2
+p opposite_count([ 21, -23, 24 -12, 23 ]) # => 1
