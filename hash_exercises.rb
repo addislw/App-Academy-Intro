bacf224e7c5f168f088fcc1615fa38932f08f394
addislw <addislw@gmail.com>
2020-12-01 20:21:08 -0500

    Practice using several different methods for initializing empty hash

--- hash_exercises.rb
+++ hash_exercises.rb
@@ -47,3 +47,49 @@ puts word_lengths("When in doubt, leave it out") #=> {"When"=>4, "in"=>2, "doubt
 
 
 
+# Write a method retrieve_values that takes in two hashes and a key. The method should return an array containing the values from the two hashes that correspond with the given key
+
+def retrieve_values(hash1, hash2, key)
+  array = []
+  array.push(hash1[key], hash2[key])
+
+end
+
+dog1 = {"name"=>"Fido", "color"=>"brown"}
+dog2 = {"name"=>"Spot", "color"=> "white"}
+print retrieve_values(dog1, dog2, "name") #=> ["Fido", "Spot"]
+puts
+print retrieve_values(dog1, dog2, "color") #=> ["brown", "white"]
+puts
+
+
+
+# Write a method cat_builder that takes in a name, color, and age. The method should return a hash representing a cat with those values
+
+def cat_builder(name, color, age)
+  cat = {'name' => name, color: color, age: age}
+end
+
+print cat_builder("Whiskers", "orange", 3) #=> {"name"=>"Whiskers", "color"=>"orange", "age"=>3}
+puts
+
+print cat_builder("Salem", "black", 100) #=> {"name"=>"Salem", "color"=>"black", "age"=>100}
+puts
+
+
+
+# Write a method ae_count that takes in a string and returns a hash containing the number of a's and e's in the string. Assume the string contains only lowercase characters
+
+def ae_count(string)
+  hash = Hash.new(0)
+  string.each_char do |char|
+    if char == 'a' || char == 'e'
+      hash[char] += 1 
+    end
+  end
+
+  hash
+end
+
+puts ae_count("everyone can program") #=> {"a"=>2, "e"=>3}
+puts ae_count("keyboard") #=> {"a"=>1, "e"=>1}
