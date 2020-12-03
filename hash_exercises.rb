5b33f2c0edd61c9b9e3cca3b1a81dcaac45e3ae4
addislw <addislw@gmail.com>
2020-12-03 18:30:59 -0500

    Hash exercises completed

--- hash_exercises.rb
+++ hash_exercises.rb
@@ -190,3 +190,42 @@ puts
 
 
 
+# Write a method unique_elements that takes in an array and returns a new array where all duplicate elements are removed. Solve this using a hash
+
+def unique_elements(array)
+  hash = Hash.new(0)
+  array.each { |ele| hash[ele] += 1 }
+
+  hash.keys
+end
+
+print unique_elements(['a', 'b', 'a', 'a', 'b', 'c']) #=> ["a", "b", "c"]
+puts
+
+
+
+# Write a method element_replace that takes in an array and a hash. The method should return a new array where elements of the original array are replaced with their corresponding values in the hash
+
+def element_replace(array, hash)
+  new_array = []
+
+  array.each do |ele|
+    if hash.keys.include?(ele)
+      new_array << hash[ele]
+    else
+      new_array << ele
+    end
+  end
+
+    new_array
+end
+
+arr1 = ["LeBron James", "Lionel Messi", "Serena Williams"]
+hash1 = {"Serena Williams"=>"tennis", "LeBron James"=>"basketball"}
+print element_replace(arr1, hash1) # => ["basketball", "Lionel Messi", "tennis"]
+puts
+
+arr2 = ["dog", "cat", "mouse"]
+hash2 = {"dog"=>"bork", "cat"=>"meow", "duck"=>"quack"}
+print element_replace(arr2, hash2) # => ["bork", "meow", "mouse"]
+puts
