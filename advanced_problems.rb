31b6fdc748dda078b5a33af284b4a0db4c70a732
addislw <addislw@gmail.com>
2020-12-03 23:42:10 -0500

    Create method that accepts a string and returns an array of the elements containing 'o'

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -21,3 +21,55 @@ countries = [
 ]
 print map_by_name(countries) #=> ["Japan", "Hungary", "Kenya"]
 puts
+
+
+
+# Write a method map_by_key that takes in an array of hashes and a key string. The method should returns a new array containing the values from each hash for the given key
+
+def map_by_key(hash_array, key)
+  hash_array.map { |hash| hash[key] }
+end
+
+locations = [
+  {"city"=>"New York City", "state"=>"New York", "coast"=>"East"},
+  {"city"=>"San Francisco", "state"=>"California", "coast"=>"West"},
+  {"city"=>"Portland", "state"=>"Oregon", "coast"=>"West"},
+]
+
+print map_by_key(locations, "state") #=> ["New York", "California", "Oregon"]
+puts
+print map_by_key(locations, "city") #=> ["New York City", "San Francisco", "Portland"]
+puts
+
+
+
+# Write a method yell_sentence that takes in a sentence string and returns a new sentence where every word is yelled. See the examples. Use map to solve this
+
+def yell_sentence(string)
+  string.split.map { |word| "#{word.upcase}!" }
+  .join(' ')
+end
+
+puts yell_sentence("I have a bad feeling about this") #=> "I! HAVE! A! BAD! FEELING! ABOUT! THIS!"
+
+
+
+# Write a method whisper_words that takes in an array of words and returns a new array containing a whispered version of each word. See the examples. Solve this using map
+
+def whisper_words(word_array)
+  word_array.map { |word| "#{word.downcase}..." }
+end
+
+print whisper_words(["KEEP", "The", "NOISE", "down"]) # => ["keep...", "the...", "noise...", "down..."]
+puts
+
+
+
+# Write a method o_words that takes in a sentence string and returns an array of the words that contain an "o". Use select in your solution
+
+def o_words(string)
+  string.split.select { |word| word.include?('o') }
+end
+
+print o_words("How did you do that?") #=> ["How", "you", "do"]
+puts
