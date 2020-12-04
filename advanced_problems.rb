50598e89adbaa08b8abc6c465dbf06095ba79135
addislw <addislw@gmail.com>
2020-12-04 03:09:41 -0500

    Method prime? created that accepts a number argument and returns boolean indicating if prime

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -118,3 +118,19 @@ end
 
 print most_vowels("what a wonderful life") #=> "wonderful"
 puts
+
+
+
+# Write a method prime? that takes in a number and returns a boolean, indicating whether the number is prime. A prime number is only divisible by 1 and itself
+
+def prime?(num)
+  return false if num <= 1
+  (2...num).none? { |factor| num % factor == 0 }
+end
+
+puts prime?(2)  #=> true
+puts prime?(5)  #=> true
+puts prime?(11) #=> true
+puts prime?(4)  #=> false
+puts prime?(9)  #=> false
+puts prime?(-5) #=> false
