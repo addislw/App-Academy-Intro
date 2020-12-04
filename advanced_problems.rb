506c7c412d9a9648717475a51f7b37240cdbc381
addislw <addislw@gmail.com>
2020-12-04 17:24:49 -0500

    Method created that utilizes previous prime? method to return array of prime numbers

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -134,3 +134,21 @@ puts prime?(11) #=> true
 puts prime?(4)  #=> false
 puts prime?(9)  #=> false
 puts prime?(-5) #=> false
+
+
+
+# Write a method pick_primes that takes in an array of numbers and returns a new array containing only the prime numbers
+
+def pick_primes(num_array)
+  num_array.select { |num| prime?(num) }
+end
+
+def prime?(num)
+  return false if num <= 1
+  (2...num).none? { |factor| num % factor == 0 }
+end
+
+print pick_primes([2, 3, 4, 5, 6]) #=> [2, 3, 5]
+puts
+print pick_primes([101, 20, 103, 2017]) #=> [101, 103, 2017]
+puts
