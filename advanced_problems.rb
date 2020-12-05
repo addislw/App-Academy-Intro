b47ce69d17a4c40d3295b21b52082ed847282317
addislw <addislw@gmail.com>
2020-12-05 12:54:35 -0500

    Iterative solution written for finding the first (n) fibonacci numbers

--- advanced_problems.rb
+++ advanced_problems.rb
@@ -253,7 +253,7 @@ puts
 
 
 
-# A number's summation is the sum of all positive numbers less than or equal to the number. For example: the summation of 3 is 6 because 1 + 2 + 3 = 6, the summation of 6 is 21 because 1 + 2 + 3 + 4 + 5 + 6 = 21. Write a method summation_sequence that takes in two numbers: start and length. 
+# A number's summation is the sum of all positive numbers less than or equal to the number. For example: the summation of 3 is 6 because 1 + 2 + 3 = 6, the summation of 6 is 21 because 1 + 2 + 3 + 4 + 5 + 6 = 21. Write a method summation_sequence that takes in two numbers: start and length 
 # The method should return an array containing length total elements. The first number of the sequence should be the start number. At any point, to generate the next element of the sequence we take the summation of the previous element. You can assume that length is not zero
 
 def summation_sequence(start, length)
@@ -276,4 +276,30 @@ end
 print summation_sequence(3, 4) # => [3, 6, 21, 231]
 puts
 print summation_sequence(5, 3) # => [5, 15, 120]
+puts
+
+
+
+# The fibonacci sequence is a sequence of numbers whose first and second elements are 1. To generate further elements of the sequence we take the sum of the previous two elements
+# For example the first 6 fibonacci numbers are 1, 1, 2, 3, 5, 8. Write a method fibonacci that takes in a number length and returns the fibonacci sequence up to the given length
+
+def fibonacci(n)
+  return [] if n == 0
+  return [1] if n == 1 
+
+  array = [1, 1]
+  while array.length < n
+    array << array[-1] + array[-2]
+  end
+
+  array
+end
+
+print fibonacci(0) # => []
+puts
+print fibonacci(1) # => [1]
+puts
+print fibonacci(6) # => [1, 1, 2, 3, 5, 8]
+puts
+print fibonacci(8) # => [1, 1, 2, 3, 5, 8, 13, 21]
 puts
